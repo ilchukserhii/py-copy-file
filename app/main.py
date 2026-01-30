ea02ac8c3c4dcc3b6096751cbead22f32436a284
Seroga <ilchuk.cergey@gmail.com>
2026-01-30 20:10:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    filenames = command.split(" ")
+    if len(filenames) < 3 or filenames[0] != "cp":
+        return
+    original_filename = filenames[1]
+    new_filename = filenames[2]
+    if original_filename == new_filename:
+        return
+    try:
+        with (open(original_filename, "r") as file_in,
+              open(new_filename, "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"File '{original_filename}' not found.")
