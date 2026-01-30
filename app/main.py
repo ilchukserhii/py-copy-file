c80e49a20338aa4fffb271172e2ad14c80182d2a
Seroga <ilchuk.cergey@gmail.com>
2026-01-30 20:15:58 +0200

    Solution 3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     filenames = command.split(" ")
-    if len(filenames) == 3 or filenames[0] != "cp":
+    if len(filenames) != 3 or filenames[0] != "cp":
         return
     original_filename = filenames[1]
     new_filename = filenames[2]
